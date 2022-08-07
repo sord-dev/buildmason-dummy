09927103cc67895448fb6d290de338ed424164ee
sord-dev <75338985+sord-dev@users.noreply.github.com>
2022-08-07 12:08:41 +0100

    adding team component

---
+++ src/components/Team.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import styles from '../styles/team.module.css'
+import HeadingText from './HeadingText'
+
+function Team() {
+  return (
+    <div className={styles.team}>
+        <HeadingText
+          title={"The leadership team"}
+          subtitle={"Our Team"}
+          desc={
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias, odio praesentium necessitatibus ipsam explicabo autem adipisci excepturi magnam, eius maxime illum sit animi expedita suscipit laudantium ea! Reprehenderit voluptatibus suscipit iusto dolore quos quia, doloribus, blanditiis cupiditate, fuga distinctio corporis."
+          }
+        />
+
+        <div className={styles.imageGrid}>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+  )
+}
+
+export default Team
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import HeadingText from "../components/HeadingText";
+import Team from "../components/Team";
 import styles from "../styles/about.module.css";
 
 // TODO
@@ -9,8 +10,7 @@ import styles from "../styles/about.module.css";
 function About() {
   return (
     <div className={styles.about}>
-
-    {/* now that's an easy hero image */}
+      {/* now that's an easy hero image */}
       <div className={styles.image}>
         <div className={styles.content}>
           <h1>Who we are</h1>
@@ -18,46 +18,42 @@ function About() {
       </div>
 
       <div className={styles.container}>
+        <HeadingText
+          title={"Helping clients get the projects they deserve done."}
+          subtitle={"Our Story"}
+          desc={
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias, odio praesentium necessitatibus ipsam explicabo autem adipisci excepturi magnam, eius maxime illum sit animi expedita suscipit laudantium ea! Reprehenderit voluptatibus suscipit iusto dolore quos quia, doloribus, blanditiis cupiditate, fuga distinctio corporis."
+          }
+        />
 
+        <div className={styles.stats}>
+          <p>Our numbers</p>
+          <h2>Handshake infographic mass market crowdfunding iteration.</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+            dolorum saepe architecto eum, cumque cum error quos autem ut tempora
+            minima ullam ex repellat voluptatem placeat quam aperiam aspernatur
+            fuga.
+          </p>
 
-      <HeadingText title={'Helping clients get the projects they deserve done.'} subtitle={'Our Story'}  desc={'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias, odio praesentium necessitatibus ipsam explicabo autem adipisci excepturi magnam, eius maxime illum sit animi expedita suscipit laudantium ea! Reprehenderit voluptatibus suscipit iusto dolore quos quia, doloribus, blanditiis cupiditate, fuga distinctio corporis.'} />
-
-      <div className={styles.stats}>
-        <p>Our numbers</p>
-        <h2>Handshake infographic mass market crowdfunding iteration.</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus dolorum saepe architecto eum, cumque cum error quos autem ut tempora minima ullam ex repellat voluptatem placeat quam aperiam aspernatur fuga.</p>
-
-        
-
-        <div className={styles.stats_numbers}>
-
-        <div>
-          <h3>1200</h3>
-          <p>Clients Helped</p>
+          <div className={styles.stats_numbers}>
+            <div>
+              <h3>1200</h3>
+              <p>Clients Helped</p>
+            </div>
+            <div>
+              <h3>120</h3>
+              <p>Companies worked with</p>
+            </div>
+            <div>
+              <h3>24,000</h3>
+              <p>Happy people</p>
+            </div>
+          </div>
         </div>
-        <div>
-          <h3>120</h3>
-          <p>Companies worked with</p>
-        </div>
-        <div>
-          <h3>24,000</h3>
-          <p>Happy people</p>
-        </div>
-        </div>
+
+        <Team />
       </div>
-
-
-      <div className={styles.headDesc}>
-        <p>Our Team</p>
-        <h2>The leadership team</h2>
-
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias, odio praesentium necessitatibus ipsam explicabo autem adipisci excepturi magnam, eius maxime illum sit animi expedita suscipit laudantium ea! Reprehenderit voluptatibus suscipit iusto dolore quos quia, doloribus, blanditiis cupiditate, fuga distinctio corporis.</p>
-      </div>
-
-      </div>
-
-      
-
     </div>
   );
 }
