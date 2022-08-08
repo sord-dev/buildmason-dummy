b2beef6f645598d169048a3468375d3d49841f5b
sord-dev <75338985+sord-dev@users.noreply.github.com>
2022-08-08 19:36:52 +0100

    changing todo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,7 +16,7 @@ import servicesImage from "../assets/services_image.jpg";
 
 //TODO
 
-// make testimonials customisable
+// make testimonials better
 
 // add dropdown to navbar https://www.youtube.com/watch?v=IF6k0uZuypA
 
