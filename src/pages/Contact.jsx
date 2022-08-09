aa6580a94d0a2767c0fcbeaa7a4d451995778448
sord-dev <75338985+sord-dev@users.noreply.github.com>
2022-08-09 12:59:08 +0100

    form styles

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import styles from "../styles/contact.module.css";
 
+// NOTE 
+// -- THIS FORM ISN'T FUNCTIONAL BECAUSE THIS IS A FICTIONAL COMPANY --
+
 function Contact() {
   return (
     <div className={styles.contact}>
@@ -8,23 +11,31 @@ function Contact() {
         <div className={styles.boxes}>
           <div>
             <h2>Phone</h2>
+            <h3>02073619831</h3>
+            <span>add link to the div</span>
           </div>
           <div>
             <h2>Email</h2>
+            <h3>someone@somewhere.com</h3>
+            <span>add link to the div</span>
           </div>
           <div>
             <h2>Location</h2>
+            <h3>Random Postcode</h3>
+            <p>Mon - Fri 6am till 9pm</p>
+            <span>add link to the div</span>
           </div>
         </div>
 
         <div className={styles.form_container}>
           <form>
             <h2>Need our Help?</h2>
+            <p>Tell us a bit about your issue in this form and we'll get back to you within the day!</p>
 
-            <input type="text" name="name" placeholder="Enter Name" />
-            <input type="text" name="name" placeholder="Enter Email" />
+            <input type="text" name="name" placeholder="Name" />
+            <input type="text" name="email" placeholder="Email" />
 
-            <textarea />
+            <textarea placeholder="Message" />
           </form>
         </div>
       </div>
